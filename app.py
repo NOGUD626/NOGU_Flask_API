fc9d6a4fc88ec79ee850efc450520afe4bdddd72
野口大輝 <noguchidaiki@noguchidaikinoMacBook-Pro.local>
2020-04-23 15:59:28 +0900

    s commit

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ SAVE_DIR = "./images"
 if not os.path.isdir(SAVE_DIR):
     os.mkdir(SAVE_DIR)
 
+port = int(os.environ.get('PORT', 5000))
+
 app = Flask(__name__)
 CORS(app)
 
@@ -23,4 +25,4 @@ def send_js(path):
     return send_from_directory(SAVE_DIR, path)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=5000,threaded=True,debug=True)
+    app.run(host='0.0.0.0',port=port,threaded=True,debug=True)
